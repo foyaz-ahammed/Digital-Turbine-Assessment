5f491d48cad410509d5e11fd55d0e71b14b786a8
Kou Thao <kouthao0916@gmail.com>
2022-04-20 08:01:18 -0700

    Declare versions, use view binding

--- app/src/main/java/com/digitalturbine/assessment/MainActivity.kt
+++ app/src/main/java/com/digitalturbine/assessment/MainActivity.kt
@@ -2,10 +2,20 @@ package com.digitalturbine.assessment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.digitalturbine.assessment.databinding.ActivityMainBinding
 
+/**
+ * Main Activity
+ * @see [R.layout.activity_main]
+ */
 class MainActivity : AppCompatActivity() {
+    // Binding variable
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
