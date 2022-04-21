b7c8871638d81dcc455b7e09897e0e13e46a6a19
Kou Thao <kouthao0916@gmail.com>
2022-04-20 17:31:08 -0700

    Add progress bar as a loading view

--- app/src/main/java/com/digitalturbine/assessment/fragments/AdListFragment.kt
+++ app/src/main/java/com/digitalturbine/assessment/fragments/AdListFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.digitalturbine.assessment.adapters.AdListAdapter
 import com.digitalturbine.assessment.databinding.FragmentAdListBinding
+import com.digitalturbine.assessment.repository.entities.LoadResult
 import com.digitalturbine.assessment.viewModels.AdListViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -36,6 +38,8 @@ class AdListFragment: Fragment() {
             adapter.submitList(it)
         }
         viewModel.loading.observe(viewLifecycleOwner) {
+            binding.progressBar.isVisible = it == LoadResult.LOADING
+            binding.recyclerView.isVisible = it == LoadResult.SUCCESS
         }
 
         viewModel.loadData()
